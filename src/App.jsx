1ef57329ead270204638d15095de9b423ad31f4f
Benni-beep <krikelkr4kel@gmail.com>
2023-08-22 09:22:21 +0200

    fix: change template to react syntax

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
       Order Summary You can now listen to millions of songs, audiobooks, and
       podcasts on any device anywhere you like! Annual Plan $59.99/year Change
       Proceed to Payment Cancel Order
-      <div class="attribution">
+      <div className="attribution">
         Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
           Frontend Mentor
         </a>
         . Coded by <a href="#">Your Name Here</a>.
