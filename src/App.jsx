9ab9959769f0a76a4dd84136b3eb5b112d3667fa
Benni-beep <krikelkr4kel@gmail.com>
2023-08-23 17:59:48 +0200

    refactor: html structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      Order Summary You can now listen to millions of songs, audiobooks, and
-      podcasts on any device anywhere you like! Annual Plan $59.99/year Change
-      Proceed to Payment Cancel Order
-      <div className="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
+    <div className="container">
+      <img src="src/assets/illustration-hero.svg" alt="dancing person" />
+      <p>Order Summary</p>
+      <p>
+        You can now listen to millions of songs, audiobooks, and podcasts on any
+        device anywhere you like!
+      </p>
+      <p className="plan-selection">
+        <img src="src/assets/icon-music.svg" alt="icon musical note" />
+        Annual Plan
+        <div>$59.99/year</div>
+        <div>Change</div>
+      </p>
+      <div className="payment-button">Proceed to Payment</div>
+      <div> Cancel Order</div>
     </div>
   );
 }
